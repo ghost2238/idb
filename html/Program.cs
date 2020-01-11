0c3f50331c5b3e95a925ae2ce69fc8dad5e1d1f7
Markus <ghsthack@gmail.com>
2020-01-11 22:52:54 +0100

    Some code to dump row output from --names to a HTML template

---
+++ html/Program.cs
@@ -0,0 +1,31 @@
+﻿using lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace html
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var dir = Directory.GetCurrentDirectory();
+            while(true)
+            {
+                if (File.Exists(dir + "/template.html"))
+                    break;
+                dir = Path.GetFullPath($"{dir}/../");
+            }
+
+            var idb = new IDBLib(dir);
+            var s = new List<string>();
+            var names = idb.GetNames($"{dir}/f2_res.idb").ToList();
+            foreach (var n in names)
+                s.Add($"<tr><td>{n.ToHex()}</td><td>{n.name}</td></tr>");
+            var template = File.ReadAllText($"{dir}/template.html");
+            File.WriteAllText($"{dir}/index.html", template.Replace("%ROWS%", string.Join("\n", s))); 
+        }
+    }
+}
---
+++ lib/lib.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace lib
+{
+    public class Symbol
+    {
+        public int address;
+        public string name;
+        public string ToHex() => "0x"+address.ToString("X");
+    }
+
+    public class IDBLib
+    {
+        string bin;
+
+        public IDBLib(string dir)
+        {
+            bin = Path.Combine(dir, "idbtool.exe");
+        }
+
+        public IEnumerable<Symbol> GetNames(string db)
+        {
+            var proc = RunArgs($"--names {db}");
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                if (line.IndexOf(':') == -1 || line.Contains("==>"))
+                    continue;
+
+                var spl = line.Split(':');
+
+                yield return new Symbol
+                {
+                    address = Convert.ToInt32(spl[0], 16),
+                    name = spl[1]
+                };
+            }
+        }
+
+        public void GetEnums(string db)
+        {
+            var proc = RunArgs($"--enums {db}");
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+
+        private Process RunArgs(string args)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = bin,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            return proc;
+        }
+    }
+}
